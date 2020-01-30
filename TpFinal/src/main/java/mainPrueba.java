3544af4f2e07a92f9753098a9d37139f77d71395
gian2782cortese@gmail.com <51422175+Straper@users.noreply.github.com>
2020-01-30 18:45:30 -0300

    Create mainPrueba.java

---
+++ TpFinal/src/main/java/mainPrueba.java
@@ -0,0 +1,47 @@
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import modelo.TipoReclamo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xx-gi
+ */
+public class mainPrueba {
+    
+    private static EntityManager manager;
+    
+    private static EntityManagerFactory emf;
+    
+    
+    
+    public static void main(String[]args) {
+        
+        emf=Persistence.createEntityManagerFactory("persistencia");
+        manager=emf.createEntityManager();
+        
+        TipoReclamo tr = new TipoReclamo (100,"master en todo");
+        
+        manager.getTransaction().begin();
+        manager.persist(tr);
+        manager.getTransaction().commit();
+        
+        
+        
+    }
+    
+    private static void imprimirTodo(){
+        
+        
+        
+    }
+    
+    
+}
