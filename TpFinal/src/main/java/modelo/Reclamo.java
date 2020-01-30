f9879541c8026408eb8dfcea4726f337c951e698
nadialarralde <60221928+nadialarralde@users.noreply.github.com>
2020-01-30 19:46:16 -0300

    Update Reclamo.java

--- TpFinal/src/main/java/modelo/Reclamo.java
+++ TpFinal/src/main/java/modelo/Reclamo.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -48,10 +49,10 @@ public class Reclamo implements Serializable {
    @JoinColumn(name="DNI")
    private Empleado empleadoRegistro;
    
-   @Column
+   @OneToOne
+   @JoinColumn(name="IDHISTORIAL")
    private Historial historial;
    
-   @Column
    private TipoReclamo tipoReclamo;
 
    //CONTRUCTOR VACIO**********************************************************************************************
