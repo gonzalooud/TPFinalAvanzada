1010d1d39ce1ce5b177cde075c1783dacf7a0a82
Gonzalo Oudin <luis.oudin@gmail.com>
2020-01-30 19:34:01 -0300

    Update ControlCliente.java

--- TpFinal/src/main/java/controlador/ControlCliente.java
+++ TpFinal/src/main/java/controlador/ControlCliente.java
@@ -6,7 +6,9 @@
 package controlador;
 
 import controladorJPA.ClienteJpaController;
-
+import modelo.Cliente;
+import java.util.List;
+import java.util.ArrayList;
 /**
  *
  * @author gonza
@@ -15,6 +17,22 @@ public class ControlCliente {
     
     private ControlSistema controlSistema;
     private ClienteJpaController jpac = new ClienteJpaController();
-    private String mensaje=""; 
+    private String mensaje="";
+    
+    public ControlCliente(ControlSistema controlSistema){
+        this.controlSistema = controlSistema;
+    }
+    
+    /* Recuperar instancia de Cliente */
+    public Cliente getCliente(int dni){
+        Cliente clienteBuscado = null;
+        List<Cliente> listaClientes = new ArrayList<>(falta el llamado a la funcion);
+        for(Cliente c: listaClientes){
+            if(c.getDni()== dni){
+                clienteBuscado = c;
+            }
+        }
+        return clienteBuscado;
+    }
     
 }
