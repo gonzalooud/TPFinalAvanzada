c1621c5ff64cffea923d757d4499b8a174480f5c
gian2782cortese@gmail.com <51422175+Straper@users.noreply.github.com>
2020-02-13 11:40:45 -0300

    cambios en implementacion

--- TpFinal/src/main/java/mainPrueba.java
+++ TpFinal/src/main/java/mainPrueba.java
@@ -39,11 +39,18 @@ public class mainPrueba {
         Query query=manager.createQuery("FROM Historial");
         List<Historial> list=query.getResultList();
         List<Integer> ultimos=new ArrayList<>();
+        List<String> todos=new ArrayList<>();
+        List<Integer> todosInt=new ArrayList<>();
         list.forEach((e) -> {
             ultimos.add(Integer.parseInt(e.getEmpleadosAsignados().get(e.getEmpleadosAsignados().size()-1).substring(2, 10 )));
+            todos.addAll(e.getEmpleadosAsignados());
             System.out.println("ultima entrada en empleados asignados :"+e.getEmpleadosAsignados().get(e.getEmpleadosAsignados().size()-1));
         });
+        todos.forEach((a)  ->{
+            todosInt.add(Integer.parseInt(a.substring(2, 10)));
+        });
             System.out.println(ultimos);
+            System.out.println(todosInt);
             int aux1=0 ,aux2 = 0, cont1=0 , cont2=0 ;
             // seleccion del que menos reclamos asignados tenga ( osea el urgente)
             for(int e:ultimos){
@@ -73,7 +80,33 @@ public class mainPrueba {
                 }
         }
             System.out.println("el empelado con menos reclamos es:"+aux1);
-            
+            for(int e:todosInt){
+                if(cont1 == 0){
+                    aux1 = e;
+                }else if(cont2 == 0){
+                    aux2=e;
+                    }else if(cont1 >= cont2){
+                        aux2=e;
+                        cont2=0;
+                        }else{
+                        aux1=aux2;
+                        cont1=cont2;
+                        aux2=e;
+                        cont2=0; 
+                        }
+                for(int a:todosInt){
+                    if(aux2 == 0){
+                        if(aux1 == a){
+                            cont1++;
+                        }
+                    }else{
+                        if(aux2 == a){
+                            cont2++;
+                        }
+                    }
+                }
+        }
+            System.out.println("el empelado con mas reclamos es:"+aux1);
             
 
         //TipoReclamo tr = new TipoReclamo ("master en todo");
