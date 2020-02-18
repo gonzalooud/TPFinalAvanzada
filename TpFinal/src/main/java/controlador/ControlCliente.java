d039507cef1db04657c01e96fd10ca997e73ad77
gian2782cortese@gmail.com <51422175+Straper@users.noreply.github.com>
2020-02-18 10:24:47 -0300

    Update ControlCliente.java

--- TpFinal/src/main/java/controlador/ControlCliente.java
+++ TpFinal/src/main/java/controlador/ControlCliente.java
@@ -4,6 +4,7 @@ import controladorJPA.ClienteJpaController;
 import modelo.Cliente;
 import java.util.List;
 import java.util.ArrayList;
+import modelo.Reclamo;
 
 public class ControlCliente {
     
@@ -14,6 +15,38 @@ public class ControlCliente {
     public ControlCliente(ControlSistema controlSistema){
         this.controlSistema = controlSistema;
     }
+    //Consulta de todos los reclamos de un cliente*********************************************************************
+    
+    private List<Integer> listarReclamos(int dniCliente){
+        List<Integer> numerosReclamo=new ArrayList<>();
+        List<Reclamo> reclamos=controlSistema.getmSistema().getReclamos();
+        for(Reclamo e:reclamos) {
+            if(e.getCliente().getDni() == dniCliente){
+                numerosReclamo.add(e.getIdReclamo());
+            }
+        }
+        return numerosReclamo;
+    }
+    
+    //Enviar todos los datos de un reclamo*****************************************************************************
+    
+     private List<String> datosReclamo(int numeroReclamo){
+        List<String>datos=new ArrayList<>();
+        List<Reclamo> reclamos=controlSistema.getmSistema().getReclamos();
+        for(Reclamo e:reclamos) {
+            if(e.getIdReclamo()== numeroReclamo){
+                datos.add("Numero Reclamo:"+String.valueOf(numeroReclamo));
+                datos.add("Servicio:"+e.getServicio().getDescripcion());
+                datos.add("Descripcion:"+e.getDescripcion());
+                datos.add("urgencia:"+e.getUrgencia());
+                datos.add("Fecha:"+e.getFecha().toString());
+                datos.add("Empelado Registro:"+e.getEmpleadoRegistro().getNombre());
+                datos.add("Estado:"+e.getHistorial().getEstado().getDescripcion());
+                datos.add("Tipo Reclamo:"+e.getTipoReclamo().getDescripcion());
+            }
+        }
+        return datos;
+     }
     
     //Recuperar instancia de Cliente***********************************************************************************
     
