ceae9c5c06fdc76d3fb23181b8ba47b647a1f3ce
Gonzalo Oudin <luis.oudin@gmail.com>
2020-02-18 19:27:08 -0300

    Revert "Revert "Merge branch 'masterv2' of https://github.com/gonzalooud/TPFinalAvanzada into masterv2""
    This reverts commit 3e9d07c16710a355acd63f3f9d29a0d8ee26d2be.

--- TpFinal/src/main/java/mainPrueba.java
+++ TpFinal/src/main/java/mainPrueba.java
@@ -38,78 +38,78 @@ public class mainPrueba {
         manager=emf.createEntityManager();
         
           
-        Query query=manager.createQuery("FROM Historial");
-        List<Historial> list=query.getResultList();
-        List<Integer> ultimos=new ArrayList<>();
-        List<String> todos=new ArrayList<>();
-        List<Integer> todosInt=new ArrayList<>();
-        list.forEach((e) -> {
-            ultimos.add(Integer.parseInt(e.getEmpleadosAsignados().get(e.getEmpleadosAsignados().size()-1).substring(2, 10 )));
-            System.out.println(Integer.parseInt(e.getEmpleadosAsignados().get(e.getEmpleadosAsignados().size()-1).substring(0, 1 )));
-            todos.addAll(e.getEmpleadosAsignados());
-            System.out.println("ultima entrada en empleados asignados :"+e.getEmpleadosAsignados().get(e.getEmpleadosAsignados().size()-1));
-        });
-        todos.forEach((a)  ->{
-            todosInt.add(Integer.parseInt(a.substring(2, 10)));
-        });
-            System.out.println(ultimos);
-            System.out.println(todosInt);
-            int aux1=0 ,aux2 = 0, cont1=0 , cont2=0 ;
-            // seleccion del que menos reclamos asignados tenga ( osea el urgente)
-            for(int e:ultimos){
-                if(cont1 == 0){
-                    aux1 = e;
-                }else if(cont2 == 0){
-                    aux2=e;
-                    }else if(cont1 <= cont2){
-                        aux2=e;
-                        cont2=0;
-                        }else{
-                        aux1=aux2;
-                        cont1=cont2;
-                        aux2=e;
-                        cont2=0; 
-                        }
-                for(int a:ultimos){
-                    if(aux2 == 0){
-                        if(aux1 == a){
-                            cont1++;
-                        }
-                    }else{
-                        if(aux2 == a){
-                            cont2++;
-                        }
-                    }
-                }
-        }
-            System.out.println("el empelado con menos reclamos es:"+aux1);
-            for(int e:todosInt){
-                if(cont1 == 0){
-                    aux1 = e;
-                }else if(cont2 == 0){
-                    aux2=e;
-                    }else if(cont1 >= cont2){
-                        aux2=e;
-                        cont2=0;
-                        }else{
-                        aux1=aux2;
-                        cont1=cont2;
-                        aux2=e;
-                        cont2=0; 
-                        }
-                for(int a:todosInt){
-                    if(aux2 == 0){
-                        if(aux1 == a){
-                            cont1++;
-                        }
-                    }else{
-                        if(aux2 == a){
-                            cont2++;
-                        }
-                    }
-                }
-        }
-            System.out.println("el empelado con mas reclamos es:"+aux1);
+//        Query query=manager.createQuery("FROM Historial");
+//        List<Historial> list=query.getResultList();
+//        List<Integer> ultimos=new ArrayList<>();
+//        List<String> todos=new ArrayList<>();
+//        List<Integer> todosInt=new ArrayList<>();
+//        list.forEach((e) -> {
+//            ultimos.add(Integer.parseInt(e.getEmpleadosAsignados().get(e.getEmpleadosAsignados().size()-1).substring(2, 10 )));
+//            System.out.println(Integer.parseInt(e.getEmpleadosAsignados().get(e.getEmpleadosAsignados().size()-1).substring(0, 1 )));
+//            todos.addAll(e.getEmpleadosAsignados());
+//            System.out.println("ultima entrada en empleados asignados :"+e.getEmpleadosAsignados().get(e.getEmpleadosAsignados().size()-1));
+//        });
+//        todos.forEach((a)  ->{
+//            todosInt.add(Integer.parseInt(a.substring(2, 10)));
+//        });
+//            System.out.println(ultimos);
+//            System.out.println(todosInt);
+//            int aux1=0 ,aux2 = 0, cont1=0 , cont2=0 ;
+//            // seleccion del que menos reclamos asignados tenga ( osea el urgente)
+//            for(int e:ultimos){
+//                if(cont1 == 0){
+//                    aux1 = e;
+//                }else if(cont2 == 0){
+//                    aux2=e;
+//                    }else if(cont1 <= cont2){
+//                        aux2=e;
+//                        cont2=0;
+//                        }else{
+//                        aux1=aux2;
+//                        cont1=cont2;
+//                        aux2=e;
+//                        cont2=0; 
+//                        }
+//                for(int a:ultimos){
+//                    if(aux2 == 0){
+//                        if(aux1 == a){
+//                            cont1++;
+//                        }
+//                    }else{
+//                        if(aux2 == a){
+//                            cont2++;
+//                        }
+//                    }
+//                }
+//        }
+//            System.out.println("el empelado con menos reclamos es:"+aux1);
+//            for(int e:todosInt){
+//                if(cont1 == 0){
+//                    aux1 = e;
+//                }else if(cont2 == 0){
+//                    aux2=e;
+//                    }else if(cont1 >= cont2){
+//                        aux2=e;
+//                        cont2=0;
+//                        }else{
+//                        aux1=aux2;
+//                        cont1=cont2;
+//                        aux2=e;
+//                        cont2=0; 
+//                        }
+//                for(int a:todosInt){
+//                    if(aux2 == 0){
+//                        if(aux1 == a){
+//                            cont1++;
+//                        }
+//                    }else{
+//                        if(aux2 == a){
+//                            cont2++;
+//                        }
+//                    }
+//                }
+//        }
+//            System.out.println("el empelado con mas reclamos es:"+aux1);
             
 
         //TipoReclamo tr = new TipoReclamo ("master en todo");
@@ -117,9 +117,12 @@ public class mainPrueba {
         manager.persist(tr);
         manager.getTransaction().commit();*/
         
-        Query queryy=manager.createQuery("FROM Reclamo"); 
-        List<Reclamo>ordenados=queryy.getResultList();
-        System.out.println(ordenados);
+        
+        Query query=manager.createQuery("FROM Reclamo r ORDER BY r.fecha");
+        List<Reclamo>ordenados=query.getResultList(); 
+        for(Reclamo e:ordenados){
+            System.out.println("NumeroReclamo: "+e.getIdReclamo()+" Urgencia: "+e.getUrgencia()+" Fecha: "+e.getFecha()+" Estado: "+e.getHistorial().getEstado().getDescripcion());
+        }
         
     }
     
